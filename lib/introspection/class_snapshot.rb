5fab7a783214400078c7d4b93182ac158b5e5134
James Mead <james@floehopper.org>
2011-06-10 15:45:56 +0100

    We can simplify the implementation of the ClassSnapshot by selecting methods based on their owner as we do in the InstanceSnapshot. This also has the benefit of making the two Snapshot classes more similar.

--- lib/introspection/class_snapshot.rb
+++ lib/introspection/class_snapshot.rb
@@ -11,9 +11,9 @@ module Introspection
           meta_ancestor.private_instance_methods(false).map { |method| Method.new(meta_ancestor, method, :private) }
         end.flatten +
         klass.ancestors.map do |ancestor|
-          (ancestor.metaclass.public_instance_methods(false) - ancestor.metaclass.ancestors.map { |a| a.public_instance_methods(false) }.flatten - (ancestor.ancestors - [ancestor]).map { |a| a.public_methods(false) }).flatten.map { |method| Method.new(ancestor, method, :public) } +
-          (ancestor.metaclass.protected_instance_methods(false) - ancestor.metaclass.ancestors.map { |a| a.protected_instance_methods(false) }.flatten - (ancestor.ancestors - [ancestor]).map { |a| a.protected_methods(false) }).flatten.map { |method| Method.new(ancestor, method, :protected) } +
-          (ancestor.metaclass.private_instance_methods(false) - ancestor.metaclass.ancestors.map { |a| a.private_instance_methods(false) }.flatten - (ancestor.ancestors - [ancestor]).map { |a| a.private_methods(false) }).flatten.map { |method| Method.new(ancestor, method, :private) }
+          ancestor.metaclass.public_instance_methods(false).select { |method| ancestor.metaclass.instance_method(method).owner == ancestor.metaclass }.map { |method| Method.new(ancestor, method, :public) } +
+          ancestor.metaclass.protected_instance_methods(false).select { |method| ancestor.metaclass.instance_method(method).owner == ancestor.metaclass }.map { |method| Method.new(ancestor, method, :protected) } +
+          ancestor.metaclass.private_instance_methods(false).select { |method| ancestor.metaclass.instance_method(method).owner == ancestor.metaclass }.map { |method| Method.new(ancestor, method, :private) }
         end.flatten
       )
     end
