765d56ca2537e46319a5ef24a92594503c5285ac
James Mead <james@floehopper.org>
2011-07-03 12:16:42 +0100

    Added samples which demonstrate the receiver chain in action.

---
+++ samples/class.rb
@@ -0,0 +1,136 @@
+require "rubygems"
+require "bundler/setup"
+require "metaid"
+
+module Kernel
+  def foo
+    puts "Kernel#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+class Object
+  def foo
+    puts "Object#foo"
+    super
+  rescue NoMethodError
+  end
+  def self.foo
+    puts "Object.foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+class Module
+  def foo
+    puts "Module#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+class Class
+  def foo
+    puts "Class#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+module GreatUncle
+  def foo
+    puts "GreatUncle#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+module GreatAunt
+  def foo
+    puts "GreatAunt#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+class Grandaddy
+  def self.foo
+    puts "Grandaddy.foo"
+    super
+  rescue NoMethodError
+  end
+  extend GreatUncle, GreatAunt
+end
+
+module Uncle
+  def foo
+    puts "Uncle#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+module Aunt
+  def foo
+    puts "Aunt#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+class Daddy < Grandaddy
+  def self.foo
+    puts "Daddy.foo"
+    super
+  rescue NoMethodError
+  end
+  extend Uncle, Aunt
+end
+
+module Brother
+  def foo
+    puts "Brother#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+module HalfSister
+  def foo
+    puts "HalfSister#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+module Sister
+  def foo
+    puts "Sister#foo"
+    super
+  rescue NoMethodError
+  end
+  include HalfSister
+end
+
+class Sonny < Daddy
+  def self.foo
+    puts "Sonny.foo"
+    super
+  rescue NoMethodError
+  end
+  extend Brother, Sister
+end
+
+Sonny.foo
+
+ancestors = []
+klass = Sonny
+while klass
+  ancestors << klass.metaclass
+  superklass = klass.respond_to?(:superclass) ? klass.superclass : nil
+  ancestors += klass.metaclass.ancestors - (superklass ? superklass.metaclass.ancestors : [])
+  klass = superklass
+end
+p ancestors
---
+++ samples/instance.rb
@@ -0,0 +1,116 @@
+require "rubygems"
+require "bundler/setup"
+require "metaid"
+
+module Kernel
+  def foo
+    puts "Kernel#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+class Object
+  def foo
+    puts "Object#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+module GreatUncle
+  def foo
+    puts "GreatUncle#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+module GreatAunt
+  def foo
+    puts "GreatAunt#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+class Grandaddy
+  def foo
+    puts "Grandaddy#foo"
+    super
+  rescue NoMethodError
+  end
+  include GreatUncle, GreatAunt
+end
+
+module Uncle
+  def foo
+    puts "Uncle#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+module Aunt
+  def foo
+    puts "Aunt#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+class Daddy < Grandaddy
+  def foo
+    puts "Daddy#foo"
+    super
+  rescue NoMethodError
+  end
+  include Uncle, Aunt
+end
+
+module Brother
+  def foo
+    puts "Brother#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+module HalfSister
+  def foo
+    puts "HalfSister#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+module Sister
+  def foo
+    puts "Sister#foo"
+    super
+  rescue NoMethodError
+  end
+  include HalfSister
+end
+
+class Sonny < Daddy
+  def foo
+    puts "Sonny#foo"
+    super
+  rescue NoMethodError
+  end
+  include Brother, Sister
+end
+
+sonny = Sonny.new
+class << sonny
+  def foo
+    puts "sonny#foo"
+    super
+  rescue NoMethodError
+  end
+end
+
+sonny.foo
+
+p [sonny.metaclass] + sonny.metaclass.ancestors
